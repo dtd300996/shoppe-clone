5e6cdcc45ce584445e335693e57bc3fe8d2be4c6
daidt7417 <daidt7417@co-well.vn>
2022-12-30 10:39:02 +0700

    config http utils

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,8 @@
-import axios, { AxiosInstance } from 'axios'
+import axios, { AxiosError, AxiosInstance } from 'axios'
+import { ResponseApi } from './../types/utils.type'
+import { toast } from 'react-toastify'
+import { isAxiosUnprocessableEntityError } from './utils'
+// import HttpStatusCode from 'src/constants/httpStatusCode.enum'
 
 class Http {
   instance: AxiosInstance
@@ -11,6 +15,27 @@ class Http {
         'Content-Type': 'application/json'
       }
     })
+
+    this.instance.interceptors.response.use(
+      function (res) {
+        return res
+      },
+      function (error: AxiosError) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        const data: any = error.response?.data
+
+        if (!isAxiosUnprocessableEntityError<ResponseApi<AxiosError>>(error)) {
+          const msg = data.message || error.message || error.message
+          toast.error(msg)
+        }
+
+        // if (error.response?.status !== HttpStatusCode.UnprocessableEntity) {
+        //   const msg = data.message || error.message
+        //   toast.error(msg)
+        // }
+        return Promise.reject(error)
+      }
+    )
   }
 }
 
---
+++ src/utils/utils.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosError } from 'axios'
+import HttpStatusCode from 'src/constants/httpStatusCode.enum'
+
+export const isAxiosError = <T>(error: unknown): error is AxiosError<T> => {
+  // eslint-disable-next-line import/no-named-as-default-member
+  return axios.isAxiosError(error)
+}
+
+// UnprocessableEntity: 422
+export const isAxiosUnprocessableEntityError = <FormError>(error: unknown): error is AxiosError<FormError> => {
+  // eslint-disable-next-line import/no-named-as-default-member
+  return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
+}
